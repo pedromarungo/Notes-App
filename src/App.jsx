2afee137f16a2a3ed3fb7145dc18d0b34f9ea804
Pedro Marungo <pedromarungo.developer@gmail.com>
2025-11-23 20:39:18 -0600

    refactor: swapped hard-coded input for updated Input component across form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import NoteForm from './components/NoteForm';
-import NoteList from './components/NoteList';
+import { useState } from "react";
+import NoteForm from "./components/NoteForm";
+import NoteList from "./components/NoteList";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   return (
-    <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadown-lg'>
-      <h2 className='text-2xl font-bold mb-4 text-center'>Notes App</h2>
+    <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadown-lg">
+      <h2 className="text-2xl font-bold mb-4 text-center">Reminder List</h2>
       <NoteForm notes={notes} setNotes={setNotes} />
       <NoteList notes={notes} />
     </div>
--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import TextInput from "./inputs/TextInput";
+import SelectInput from "./inputs/SelectInput";
 
 function NoteForm({ notes, setNotes }) {
   const [formData, setFormData] = useState({
@@ -55,39 +56,30 @@ function NoteForm({ notes, setNotes }) {
             onChange={handleChange}
             required
           />
-          <div className="mb-4">
-            <label htmlFor="priority" className="block font-semibold">
-              priority
-            </label>
-            <select
-              name="priority"
-              type="text"
-              className="w-full p-2 border rounded-lg"
-              value={formData.priority}
-              onChange={handleChange}
-            >
-              formData.
-              <option value="High">High</option>
-              <option value="Medium">Medium</option>
-              <option value="Low">Low</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="category" className="block font-semibold">
-              Category
-            </label>
-            <select
-              name="category"
-              type="text"
-              className="w-full p-2 border rounded-lg"
-              value={formData.category}
-              onChange={handleChange}
-            >
-              <option value="Work">Work</option>
-              <option value="Personal">Personal</option>
-              <option value="Ideas">Ideas</option>
-            </select>
-          </div>
+
+          <SelectInput
+            label="Priority"
+            name="priority"
+            value={formData.priority}
+            onChange={handleChange}
+            options={[
+              { value: "High", label: "High" },
+              { value: "Medium", label: "Medium" },
+              { value: "Low", label: "low" },
+            ]}
+          />
+          <SelectInput
+            label="Category"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            options={[
+              { value: "Groceries", label: "Groceries" },
+              { value: "Essentials", label: "Essentials" },
+              { value: "Wishlist", label: "Wishlist" },
+            ]}
+          />
+
           <div className="mb-4">
             <label htmlFor="description" className="block font-semibold">
               Description
