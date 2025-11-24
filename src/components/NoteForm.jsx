0728a804845bb5c65d493f9efe779599f9377df5
Pedro Marungo <pedromarungo.developer@gmail.com>
2025-11-23 19:27:06 -0600

    refactor: replace hard-coded input with reusable Input component

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import TextInput from "./inputs/TextInput";
 
 function NoteForm({ notes, setNotes }) {
   const [formData, setFormData] = useState({
-    title: '',
-    category: 'Work',
-    priority: 'Medium',
-    description: '',
+    title: "",
+    category: "Work",
+    priority: "Medium",
+    description: "",
   });
 
   const [isFormVisible, setIsFormVisible] = useState(false);
@@ -30,10 +31,10 @@ function NoteForm({ notes, setNotes }) {
 
     // reset form data
     setFormData({
-      title: '',
-      category: 'Work',
-      priority: 'Medium',
-      description: '',
+      title: "",
+      category: "Work",
+      priority: "Medium",
+      description: "",
     });
   };
 
@@ -41,70 +42,65 @@ function NoteForm({ notes, setNotes }) {
     <>
       <button
         onClick={() => setIsFormVisible(!isFormVisible)}
-        className='w-full bg-gray-100 border border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover:bg-purple-200 hover:border-purple-300 transition mb-4'
+        className="w-full bg-gray-100 border border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover:bg-purple-200 hover:border-purple-300 transition mb-4"
       >
-        {isFormVisible ? 'Hide Form' : 'Add New Note'}
+        {isFormVisible ? "Hide Form" : "Add New Note"}
       </button>
       {isFormVisible && (
-        <form onSubmit={handleSubmit} className='mb-6'>
-          <div className='mb-4'>
-            <label htmlFor='title' className='block font-semibold'>
-              Title
-            </label>
-            <input
-              name='title'
-              type='text'
-              className='w-full p-2 border rounded-lg'
-              value={formData.title}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='mb-4'>
-            <label htmlFor='priority' className='block font-semibold'>
+        <form onSubmit={handleSubmit} className="mb-6">
+          <TextInput
+            label="Title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+          />
+          <div className="mb-4">
+            <label htmlFor="priority" className="block font-semibold">
               priority
             </label>
             <select
-              name='priority'
-              type='text'
-              className='w-full p-2 border rounded-lg'
+              name="priority"
+              type="text"
+              className="w-full p-2 border rounded-lg"
               value={formData.priority}
               onChange={handleChange}
             >
               formData.
-              <option value='High'>High</option>
-              <option value='Medium'>Medium</option>
-              <option value='Low'>Low</option>
+              <option value="High">High</option>
+              <option value="Medium">Medium</option>
+              <option value="Low">Low</option>
             </select>
           </div>
-          <div className='mb-4'>
-            <label htmlFor='category' className='block font-semibold'>
+          <div className="mb-4">
+            <label htmlFor="category" className="block font-semibold">
               Category
             </label>
             <select
-              name='category'
-              type='text'
-              className='w-full p-2 border rounded-lg'
+              name="category"
+              type="text"
+              className="w-full p-2 border rounded-lg"
               value={formData.category}
               onChange={handleChange}
             >
-              <option value='Work'>Work</option>
-              <option value='Personal'>Personal</option>
-              <option value='Ideas'>Ideas</option>
+              <option value="Work">Work</option>
+              <option value="Personal">Personal</option>
+              <option value="Ideas">Ideas</option>
             </select>
           </div>
-          <div className='mb-4'>
-            <label htmlFor='description' className='block font-semibold'>
+          <div className="mb-4">
+            <label htmlFor="description" className="block font-semibold">
               Description
             </label>
             <textarea
-              name='description'
-              type='text'
-              className='w-full p-2 border rounded-lg'
+              name="description"
+              type="text"
+              className="w-full p-2 border rounded-lg"
               value={formData.description}
               onChange={handleChange}
             ></textarea>
           </div>
-          <button className='w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600'>
+          <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600">
             Create Note
           </button>
         </form>
